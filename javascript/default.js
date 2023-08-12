5546b5c23b0bdb4015d99ca63e809cd7039d74c8
AnthonissenBert <anthonissenbert78@gmail.com>
2023-08-12 22:12:48 +0200

    dupli-52: put bacchi in carousel

--- javascript/default.js
+++ javascript/default.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById("toggleNav");
-    const navList = document.getElementById("navList");
-
-    toggleButton.addEventListener("click", function() {
-        navList.classList.toggle("collapsed");
-    });
-});
+$('.carousel').carousel()
