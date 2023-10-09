732871f64dc0334ad4a864e6595e1a7139a6a649
Bert Anthonissen <bert.anthonissen@bulls-i.company>
2023-10-09 23:38:42 +0200

    mobile friendly

--- javascript/default.js
+++ javascript/default.js
@@ -1,1 +1,12 @@
-$('.carousel').carousel()
+$(document).on('resize, ready', function() {
+    // Add class if screen size equals
+    var $window = $(window),
+    $footer = $('footer');
+   
+    function resize() {
+       if ($window.width() < 992) {
+         return $footer.removeClass('fixed-bottom');
+       }
+       $window.resize(resize).trigger('resize');
+    }
+});
